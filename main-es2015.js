fa7cb6dda00439e6c65c8baa497b94afcfb9250c
Julius224 <bmolano118@gmail.com>
2020-10-01 00:05:21 -0400

    add sitekey

--- main-es2015.js
+++ main-es2015.js
@@ -267,7 +267,7 @@ AppModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineInjector
             _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
             _angular_forms__WEBPACK_IMPORTED_MODULE_27__["ReactiveFormsModule"],
             angular_google_recaptcha__WEBPACK_IMPORTED_MODULE_29__["RecaptchaModule"].forRoot({
-                siteKey: 'YOUR_SITE_KEY_HERE',
+                siteKey: '6Lf5SNIZAAAAAEe2pUSPMOmDaSUHWd-JjhjDwWXe',
             }),
         ]] });
 (function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵsetNgModuleScope"](AppModule, { declarations: [_app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"],
@@ -336,7 +336,7 @@ AppModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineInjector
                     _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
                     _angular_forms__WEBPACK_IMPORTED_MODULE_27__["ReactiveFormsModule"],
                     angular_google_recaptcha__WEBPACK_IMPORTED_MODULE_29__["RecaptchaModule"].forRoot({
-                        siteKey: 'YOUR_SITE_KEY_HERE',
+                        siteKey: '6Lf5SNIZAAAAAEe2pUSPMOmDaSUHWd-JjhjDwWXe',
                     }),
                 ],
                 providers: [],
